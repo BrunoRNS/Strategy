482d6b28f12cf077a96c41300015af952426ff0a
BrunoRNS <140832313+BrunoRNS@users.noreply.github.com>
2024-02-11 18:29:24 -0300

    Atualizar o script.js

--- Strategy/home/script.js
+++ Strategy/home/script.js
@@ -7,24 +7,24 @@ const id = "65c68d64266cfc3fde88254e";
 const api = "$2a$10$3CIY0DoGOnJ8LKdzhENZ4OnjB2v5Ww1IRA9ZQuDUE1d4O3oG5gbRK";
 
 function data(page, id = id, api = api) {
-   // An async function to get the json
-   async function getJSON(id, api) {
-     // A https request with fetch in the method GET, to get the JSON
-     let response = await fetch(`https://api.jsonbin.io/b/${id}`, {
-     headers: {
-      "secret-key": api
-     }
-     });
-     // Verify if ok
-     if (response.ok) {
-       // Convert into json
-       let data = await response.json();
-       // Return data
-       return data;
-     } else {
-       // Raise an error if failed
-       throw new Error(response.status);
-     }
+    // An async function to get the json
+    async function getJSON(id, api) {
+      // A https request with fetch in the method GET, to get the JSON
+      let response = await fetch(`https://api.jsonbin.io/b/${id}`, {
+      headers: {
+         "secret-key": api
+      }
+      });
+      // Verify if ok
+      if (response.ok) {
+         // Convert into json
+         let data = await response.json();
+         // Return data
+         return data;
+      } else {
+         // Raise an error if failed
+         throw new Error(response.status);
+      }
    }
 
    // Call the GetJSON function recursiving
@@ -34,16 +34,16 @@ function data(page, id = id, api = api) {
      let dataString = JSON.stringify(data);
      // Change Data Content
      fs.writeFile("/Strategy/data.json", dataString, (err) => {
-     // Verify if there is an error
-     if (err) {
-       // Throw the error
-       throw err;
-     } else {
-       // Success message
-       console.log("200! Success");
-     }
-   });
+        // Verify if there is an error
+        if (err) {
+           // Throw the error
+           throw err;
+        } else {
+           // Success message
+           console.log("200! Success");
+        }
+     });
    } catch (error) {
-      // If an exception happen in proccess
-      console.error(error);
+       // If an exception happen in proccess
+       console.error(error);
    }
