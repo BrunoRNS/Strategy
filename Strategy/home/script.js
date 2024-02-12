27d1a4cb6da0cff217d0b955c600288be3d1e586
BrunoRNS <140832313+BrunoRNS@users.noreply.github.com>
2024-02-12 11:43:59 -0300

    Update script.js

--- Strategy/home/script.js
+++ Strategy/home/script.js
@@ -5,43 +5,42 @@ function data(form, username, id ="65c68d64266cfc3fde88254e", api = "$2a$10$3CIY
     const https = require("https");
     // An async function to get the json
     async function getJSON(id, api) {
-      // A https request with fetch in the method GET, to get the JSON
-      let response = await fetch(`https://api.jsonbin.io/b/${id}`, {
-      headers: {
-         "secret-key": api
-      }
-      });
-      // Verify if ok
-      if (response.ok) {
-         // Convert into json
-         let data = await response.json();
-         // Return data
-         return data;
-      } else {
-         // Raise an error if failed
-         form.insertAdjacentHTML("beforeend", "<p class='erro'>Error 103.</p>");
-      }
-   }
-
-   // Call the GetJSON function recursiving
-   try {
-     let data = await getJSON(id, api);
-     // Convert in a JSON string
-     let dataString = JSON.stringify(data);
-     console.log(dataString)
-     // Change Data Content
-     fs.writeFile("/Strategy/data.json", dataString, (err) => {
-        // Verify if there is an error
-        if (err) {
-           // Throw the error             
-           form.insertAdjacentHTML("beforeend", "<p class='erro'>Error 100." + err + "</p>");
+        // A https request with fetch in the method GET, to get the JSON
+        let response = await fetch(`https://api.jsonbin.io/b/${id}`, {
+            headers: {
+                "secret-key": api
+            }
+        });
+        // Verify if ok
+        if (response.ok) {
+            // Convert into json
+            let data = await response.json();
+            // Return data
+            return data;
         } else {
-           // Success message
-           console.log("200! Success");
+            // Raise an error if failed
+            form.insertAdjacentHTML("beforeend", "<p class='erro'>Error 103.</p>");
         }
-     });
-     return data;
-   } catch (error) {
-       // If an exception happen in proccess
-       form.insertAdjacentHTML("beforeend","<p class='erro'>Error 106." + error + "</p>");
-   }
+    }
+    // Call the GetJSON function recursiving
+    try {
+        let data = await getJSON(id, api);
+        // Convert in a JSON string
+        let dataString = JSON.stringify(data);
+        console.log(dataString)
+        // Change Data Content
+        fs.writeFile("/Strategy/data.json", dataString, (err) => {
+            // Verify if there is an error
+            if (err) {
+                // Throw the error             
+                form.insertAdjacentHTML("beforeend", "<p class='erro'>Error 100." + err + "</p>");
+            } else {
+                // Success message
+                console.log("200! Success");
+            }
+        });
+        return data;
+    } catch (error) {
+        // If an exception happen in proccess
+        form.insertAdjacentHTML("beforeend","<p class='erro'>Error 106." + error + "</p>");
+    }
